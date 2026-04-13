8faf36317b8d7ba605558b32525eb744bd48fd34
davimacca95 <davi.maccarrone@gmail.com>
2026-04-13 22:30:37 +0200

    update 2026-04-13 22:30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'korea-trip-v1775734525';
+const CACHE = 'korea-trip-v1776112237';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
